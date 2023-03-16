f1865e17ea78612a76707c3440c445707946bc00
phadeline <112015433+phadeline@users.noreply.github.com>
2023-03-16 19:43:55 -0400

    started typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,17 +3,20 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type Bio {
     _id: ID!
-    interests: String!
+    interests: [String]!
     bio: String
+    age: Int
+    gender: String
+    location: String
   }
   type Query {
     bios: [Bio]
     bio(bioId: ID!): Bio
   }
   type Mutation {
-    addBio(interests: String!, bio: String!): Bio
+    addBio(interests: String!, bio: String!, age: Int, gender: String, location: String): Bio
     removeBio(bioId: ID!): Bio
-    updateBio(bioId: ID!, interests: String!, bio: String!): Bio
+    updateBio(bioId: ID!, interests: String!, bio: String!, age: Int, gender: String, location: String): Bio
   }
 `;
 
