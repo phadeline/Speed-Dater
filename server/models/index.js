871bc7bcb94f205a1e8d2473d708bad55a352cf8
Humzas <humzas@umich.edu>
2023-03-21 19:27:48 -0400

    s3 optim

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Bio = require("./Bio");
 const Preference = require("./Preference");
+//just incase we need a model
 const Image = require("./Image");
 module.exports = { User, Bio, Preference, Image };
