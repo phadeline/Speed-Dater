7dcad85c2c11d2d5f19015a65dbf40e88e04d6a0
unknown <brianna.albo@gmail.com>
2023-03-27 20:33:40 -0400

    added structure and basic styling to user dashboard page

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../../index.css'
+
 
 
 
@@ -6,7 +8,48 @@ const Dashboard = () => {
 
 
   return (
-   <h1> Dashboard</h1>
+    <container id='case'>
+   <section id='profileSnip'>
+    <div>
+    <h3 id='dashUsername'> Username</h3> 
+    </div>
+    <div id='usersPicture'></div>
+    <p id='dashGender' class='dashDetails'>gender <span id='dashAge'>, 28 </span></p>
+    <p id='dashOrientation' class='dashDetails'>straight</p>
+    <p id='dashLocation' class='dashDetails'>Belivdere, NJ</p>
+    
+    </section>
+    <section id='bioSnip' >
+      <div>
+      <h3>Bio Section</h3>
+      </div>
+      <div id='dashbioContent'>
+        <p id='dashBioSummary'>This is my SpeedDater app summary area where I describe myself 
+        to potential matches that I might meet! This is some more filler to add to the Bio to fill 
+        out the div and test how the page will look!</p>
+        <h4>Interests:</h4>
+        <p id='dashInterests'><ul>
+          <li>Skiing</li>
+          <li>Cooking</li>
+            <li>Traveling</li></ul></p>
+      </div>
+
+      </section>
+      <section id='connectionList'>
+        <div>
+        <h3> Your Matches</h3>
+        </div>
+        <div id='dashFriends'>
+          <p class='eachfriend'>friend one</p>
+          <p class='eachfriend'>friend one</p>
+          <p class='eachfriend'>friend one</p>
+          <p class='eachfriend'>friend one</p>
+          <p class='eachfriend'>friend one</p>
+        </div>
+        
+      </section>
+
+   </container>
   )
 }; 
 
