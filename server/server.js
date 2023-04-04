51958d59e9b68d063c8536d856679f933d75db33
Humzas <humzas@umich.edu>
2023-04-03 23:39:49 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -23,11 +23,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build/index.html")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"));
+  res.sendFile(path.join(__dirname, "../client/build"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
